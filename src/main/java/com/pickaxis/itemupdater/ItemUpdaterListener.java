ae18c98ffe993b37c53e3a4ddc1ac094e4095fe0
NickG365 <nick@stormjive.com>
2016-03-26 13:56:00 -0400

    Update items that are clicked or dropped to prevent merging dissimilar types.

--- src/main/java/com/pickaxis/itemupdater/ItemUpdaterListener.java
+++ src/main/java/com/pickaxis/itemupdater/ItemUpdaterListener.java
@@ -2,7 +2,9 @@ package com.pickaxis.itemupdater;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ItemUpdaterListener implements Listener
@@ -17,5 +19,17 @@ public class ItemUpdaterListener implements Listener
     public void onPlayerJoin( PlayerJoinEvent event )
     {
         ItemUpdaterPlugin.getInstance().getUpdater().updateInventory( event.getPlayer().getInventory() );
+    }
+    
+    @EventHandler
+    public void onPlayerDropItem( PlayerDropItemEvent event )
+    {
+        ItemUpdaterPlugin.getInstance().getUpdater().updateItem( event.getItemDrop().getItemStack() );
+    }
+    
+    @EventHandler
+    public void onInventoryClick( InventoryClickEvent event )
+    {
+        ItemUpdaterPlugin.getInstance().getUpdater().updateItem( event.getCurrentItem() );
     }
 }
